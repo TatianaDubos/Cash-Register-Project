03437e89c80eddd7449f2326555a4c7cde64c497
TatianaDubos <92888922+TatianaDubos@users.noreply.github.com>
2023-05-20 15:21:20 -0400

    Add files via upload

---
+++ Clavier.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace GuichetAutomatique
+{
+    internal class Clavier
+    {
+        public static void aff(object obj, string lettre) {  // Méthode pour écrire les numéros
+                                                             // saisis dans le clavier numérique à l'écran
+            
+            if (obj.GetType() == typeof(TextBox))  // Si l'objet est un TEXBOX
+            {
+                
+                TextBox text = (TextBox)obj;
+
+                if (text.MaxLength == text.Text.Length) return; // Quitter si la longueur maximum a été atteinte
+                 
+                text.Text += lettre; // Ajouter la lettre
+
+                text.Select(text.Text.Length, 0); // Déplacer le curseur
+
+            }
+            else if (obj.GetType() == typeof(PasswordBox)) // Si l'objet est PASSWORDBOX
+            {
+                PasswordBox password = (PasswordBox)obj;
+
+                if (password.MaxLength == password.Password.Length) return; // Quitter si la longueur maximum a été atteinte
+               
+                password.Password += lettre; // Ajouter la lettre
+
+                password.GetType().GetMethod("Select", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(password, new object[] { password.Password.Length, 0 }); // Déplacer le curseur
+            }
+            
+          
+        }
+    }
+}
